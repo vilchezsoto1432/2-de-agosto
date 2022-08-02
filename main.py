cbedf684648cd216fa304b02909e69f14c8f29d7
vilchezsoto1432 <107497863+vilchezsoto1432@users.noreply.github.com>
2022-08-02 12:49:03 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+productos = {"codigo": ["01","02","03","04"], "nombre": [ "Teclado", "monitor", "impresora", "audifonos", "telefono"], "precio":[100,500,400,70,600]}
+print(productos)
+
+for key in productos:
+    print(key, ":", productos[key])
+
